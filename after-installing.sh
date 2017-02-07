f84d1d378a25edb768b2e8d705913f35c6cafb70
tomastd <tomas.tamayo@gmail.com>
2017-02-07 16:41:18 +0100

    File name and LTS kernel version change

--- after-installing.sh
+++ after-installing.sh
@@ -14,7 +14,7 @@ GUI=$(zenity --list --checklist \
 	--column=Type \
 	--column=Description \
 	TRUE "Update System" "APT" "Updates the package lists, the system packages and Applications."  \
-    FALSE "Upgrade 4.4.X LTS Kernel" "DEB" "Upgrade to the last Long Term Suport 4.4.X LTS Kernel." \
+    FALSE "Upgrade 4.9.X LTS Kernel" "DEB" "Upgrade to the last Long Term Suport 4.9.X LTS Kernel." \
 	FALSE "Enable PPAs" "APT" "Another extra layer of security and another level of annoyance. You cannot add PPA by default in Loki." \
 	FALSE "Install Y-PPA-Manager" "PPA" "Tool that lets you easily add, remove or purge PPAs as well as search for packages in Launchpad PPAs." \
 	FALSE "Install Elementary Tweaks" "PPA" "Installing themes in elementary OS is a much easier task thanks to elementary Tweaks tool." \
@@ -70,15 +70,15 @@ then
 	echo ""
 	if [[ $(uname -m) == "i686" ]]
 	then
-		wget -O /tmp/all.deb            http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.4.43/linux-headers-4.4.43-040443_4.4.43-040443.201701150831_all.deb
-		wget -O /tmp/headers_i386.deb   http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.4.43/linux-headers-4.4.43-040443-generic_4.4.43-040443.201701150831_i386.deb
-		wget -O /tmp/image_i386.deb     http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.4.43/linux-image-4.4.43-040443-generic_4.4.43-040443.201701150831_amd64.deb
+		wget -O /tmp/all.deb            http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.8/linux-headers-4.9.8-040908_4.9.8-040908.201702040431_all.deb
+		wget -O /tmp/headers_i386.deb   http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.8/linux-headers-4.9.8-040908-generic_4.9.8-040908.201702040431_i386.deb
+		wget -O /tmp/image_i386.deb     http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.8/linux-image-4.9.8-040908-generic_4.9.8-040908.201702040431_i386.deb
 		sudo dpkg -i /tmp/all.deb /tmp/headers_i386.deb /tmp/image_i386.deb
 	elif [[ $(uname -m) == "x86_64" ]]
 	then
-		wget -O /tmp/all.deb            http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.4.43/linux-headers-4.4.43-040443_4.4.43-040443.201701150831_all.deb
-		wget -O /tmp/headers_amd64.deb  http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.4.43/linux-headers-4.4.43-040443-generic_4.4.43-040443.201701150831_amd64.deb
-        wget -O /tmp/image_amd64.deb    http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.4.43/linux-image-4.4.43-040443-generic_4.4.43-040443.201701150831_amd64.deb
+		wget -O /tmp/all.deb            http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.8/linux-headers-4.9.8-040908_4.9.8-040908.201702040431_all.deb
+		wget -O /tmp/headers_amd64.deb  http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.8/linux-headers-4.9.8-040908-generic_4.9.8-040908.201702040431_amd64.deb
+        	wget -O /tmp/image_amd64.deb    http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.8/linux-image-4.9.8-040908-generic_4.9.8-040908.201702040431_amd64.deb
 		sudo dpkg -i /tmp/all.deb /tmp/headers_amd64.deb /tmp/image_amd64.deb
 	fi
 	sudo update-grub
